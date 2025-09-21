4d3a4a318c3715ecd7370316ba6c3813cfe8d110
OrdonezCarol <carol.ordonez@upch.pe>
2025-09-21 12:01:19 -0500

    app.py

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ st.write("""
 #@st.cache_resource
 def load_model():
     filename = "./breast_mnist_model.h5"
-    model = pickle.load(open(filename, "breast_mnist_model.h5"))
-    #model = tf.keras.models.load_model("breast_cancer_model.h5")  # Asegúrate de que el archivo esté en la carpeta
+    #model = pickle.load(open(filename, "breast_mnist_model.h5"))
+    model = tf.keras.models.load_model("breast_mnist_model.h5")  # Asegúrate de que el archivo esté en la carpeta
     return model
 
 model = load_model()
